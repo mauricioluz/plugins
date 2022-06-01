915ec30dfe6857da6667584144ae720e2c7262a6
Chris Yang <ychris@google.com>
2022-05-31 17:33:11 -0700

    [google_maps_flutter] Objective-C code clean up (#5780)

--- packages/google_maps_flutter/google_maps_flutter/ios/Classes/FLTGoogleMapsPlugin.h
+++ packages/google_maps_flutter/google_maps_flutter/ios/Classes/FLTGoogleMapsPlugin.h
@@ -11,5 +11,9 @@
 #import "GoogleMapPolylineController.h"
 #import "GoogleMapGroundOverlayController.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface FLTGoogleMapsPlugin : NSObject <FlutterPlugin>
 @end
+
+NS_ASSUME_NONNULL_END
